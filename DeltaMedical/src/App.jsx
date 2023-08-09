9dc9b77d0316db54602f0ddb98c77cb99ee310c0
gedeastu <gede.astu@gmail.com>
2023-08-09 23:29:11 +0800

    up

--- DeltaMedical/src/App.jsx
+++ DeltaMedical/src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={{}} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
