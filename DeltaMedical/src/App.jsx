25c14598cad4de1e021a809662c1bea91e5eee94
gedeastu <gede.astu@gmail.com>
2023-08-09 23:56:31 +0800

    up

--- DeltaMedical/src/App.jsx
+++ DeltaMedical/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={{}} />
+          <Route path="/" element={<Home/>} />
         </Routes>
       </Router>
     </>
--- DeltaMedical/src/pages/Home/Home.jsx
+++ DeltaMedical/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return;
-  <></>;
+  return (
+    <>
+      <div className="bg-white text-black">
+        Hello World
+      </div>
+    </>
+  );
 };
 
 export default Home;
